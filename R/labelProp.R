00872558cf5b9a6c57fa036032d2ba83f8f14057
prodriguezsosa <pedro.rodriguezsosa@gmail.com>
2022-03-03 10:57:38 -0600

    bootstrapping and permutation

--- R/labelProp.R
+++ R/labelProp.R
@@ -20,6 +20,7 @@
 # permutation options
 # visualization
 # EXAMPLES
+# INSTANCE?
 
 #' Compute matrix of transition probabilities
 #'
@@ -35,90 +36,102 @@
 #' @keywords build_transition_matrix
 #' @examples
 #'
-labelProp <- function(seeds, x, method = "rw", metric = "cosine", beta = 0.5, verbose = TRUE){
-
+labelProp <- function(seeds, x, method = "rw", metric = "cosine", beta = 0.5, bootstrap = FALSE, num_bootstraps = 50, num_seeds = 1, permute = FALSE, num_permutations = 100, verbose = TRUE){
+  
   # check seeds are in x, if not, report and remove
   in_x <- seeds %in% rownames(x)
-
+  
   # remove from seed set
   seeds <- seeds[in_x]
-
+  
   # if no seeds are present in x, stop
-  if(length(seeds)==0) stop("none of the seeds are present in the x")
-
+  if( length(seeds)==0 ) stop("none of the seeds are present in the x")
+  
   # report seeds not in x
-  if(!all(in_x)) cat("words not in the vocabulary: ", paste(seeds[!in_x], collapse = ', '))
-
-  if(method == "rw"){
-
+  if( !all(in_x) ) cat("words not in the vocabulary: ", paste(seeds[!in_x], collapse = ', '))
+  
+  if ( method == "rw" ) {
+    
     # check x is a transition matrix
     if(!(nrow(x) == ncol(x) && all(round(rowSums(x), 0) == 1) && all(diag(x) == 0)))stop('if method = "rw", x must be a symmetric transtition matrix with rows summing to 1 and diagonal set to 0.')
-
-    if(bootstrap)
-
-    # initialize vector of scores
-    score <- matrix(rep(1/nrow(x),nrow(x)), nrow = nrow(x), ncol = 1)
-
-    # anchor vector for seeds
-    seed_anchor <- matrix(0, nrow = nrow(x), ncol = 1)
-    seed_anchor[which(colnames(x) == seeds)] <- 1/length(seed_anchor)
-
-    # iterative updating solution
-    #epsilon = 0.00000001
-    #conv_dist <- 1
-    #while(conv_dist > epsilon){
-    #  score_lag <- score
-    #  score <- beta*x%*%score_lag + (1 - beta)*seed_anchor
-    #  conv_dist <- sqrt(sum((score - score_lag)^2))
-    #  if(verbose) print(conv_dist)
-    #}
-
-    # closed-form solution
-    score <- solve(diag(nrow(x)) - beta*x)%*%seed_anchor/length(seeds)
-
-    # result
-    # TO DO: SCALE OR NOT TO SCALE
-    result <- data.frame("instance" = rownames(x), "score" = scale(score)[,1])
-  } else if(method == "nns"){
-
-    # take column average of seed embeddings
-    seed_vec = matrix(Matrix::colMeans(x[seeds,]), nrow = 1)
-
-    # compute similarity
-    sim_vec <- text2vec::sim2(x = seed_vec, y = x, method = "cosine", norm = "l2")[1,]
-
-    # result
-    result <- data.frame("instance" = names(sim_vec), "score" = scale(unname(sim_vec))[,1])
-
+    
+    if (bootstrap) {
+      
+      result <- replicate(num_bootstraps, compute_rw_score(x = x, sample(seeds, size = num_seeds, replace = FALSE)), simplify = FALSE)
+      result <- bind_rows(result) %>% group_by(instance) %>% summarize(value = mean(score), std.error = sd(score)) %>% ungroup()
+    
+      } else {
+        
+      result <- compute_score(x = x, seeds = seeds) %>% select(instance, value = score)
+   
+      }
+    
+    if ( permute ){
+      
+      perm_result <- replicate(num_permutations, compute_rw_score(x = x, sample(rownames(x), size = length(seeds), replace = FALSE)), simplify = FALSE)
+      perm_result <- lapply(perm_result, function(i) result$value > i$score)
+      result$p.value <- 1 - rowSums(do.call(cbind, perm_result))/length(perm_result)
+      
+    }
+    
+  } else if ( method == "nns" ) {
+    
+    if (bootstrap) {
+      
+      result <- replicate(num_bootstraps, compute_nns_score(x = x, sample(seeds, size = num_seeds, replace = FALSE)), simplify = FALSE)
+      result <- bind_rows(result) %>% group_by(instance) %>% summarize(value = mean(score), std.error = sd(score)) %>% ungroup()
+      
+    } else {
+      
+      result <- compute_nns_score(x = x, seeds = seeds) %>% select(instance, value = score)
+      
+    }
+    
+    if ( permute ){
+      
+      perm_result <- replicate(num_permutations, compute_nns_score(x = x, sample(rownames(x), size = length(seeds), replace = FALSE)), simplify = FALSE)
+      perm_result <- lapply(perm_result, function(i) result$value > i$score)
+      result$p.value <- 1 - rowSums(do.call(cbind, perm_result))/length(perm_result)
+      
+    }
+    
   } else stop('method must be either "rw" or "nns"')
-
+  
   # output
   return(result)
 }
 
-compute_score <- function(x){
+compute_rw_score <- function(x, seeds){
+  
   # initialize vector of scores
   score <- matrix(rep(1/nrow(x),nrow(x)), nrow = nrow(x), ncol = 1)
-
+  
   # anchor vector for seeds
   seed_anchor <- matrix(0, nrow = nrow(x), ncol = 1)
-  seed_anchor[which(colnames(x) == seeds)] <- 1/length(seed_anchor)
-
-  # iterative updating solution
-  #epsilon = 0.00000001
-  #conv_dist <- 1
-  #while(conv_dist > epsilon){
-  #  score_lag <- score
-  #  score <- beta*x%*%score_lag + (1 - beta)*seed_anchor
-  #  conv_dist <- sqrt(sum((score - score_lag)^2))
-  #  if(verbose) print(conv_dist)
-  #}
+  seed_anchor[which(colnames(x) %in% seeds)] <- 1/length(seed_anchor)
 
   # closed-form solution
   score <- solve(diag(nrow(x)) - beta*x)%*%seed_anchor/length(seeds)
-
+  score <- scale(score)[,1]
+  
   # result
-  # TO DO: SCALE OR NOT TO SCALE
-  result <- data.frame("instance" = rownames(x), "score" = scale(score)[,1])
+  result <- data.frame("instance" = names(score), "score" = unname(score))
+  
+  # out
   return(result)
+}
+
+compute_nns_score <- function(x, seeds){
+  
+# take column average of seed embeddings
+seed_vec = matrix(Matrix::colMeans(matrix(x[seeds,], nrow = length(seeds), ncol = ncol(x))), nrow = 1)
+
+# compute similarity
+sim_vec <- text2vec::sim2(x = seed_vec, y = x, method = "cosine", norm = "l2")[1,]
+
+# result
+result <- data.frame("instance" = names(sim_vec), "score" = scale(unname(sim_vec))[,1])
+
+# out
+return(result)
 }
