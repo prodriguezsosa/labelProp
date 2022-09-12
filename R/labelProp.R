9371c1dbfd5f62a6e5ec6d97b97441d56b693b2d
Pedro L. Rodríguez <pedro.rodriguezsosa@gmail.com>
2022-09-12 12:07:31 -0700

    add permutation test

--- R/labelProp.R
+++ R/labelProp.R
@@ -7,7 +7,6 @@
 #' @param seeds (list) a list of character vectors defining the classes of interest.
 #' If named, then `names(seeds)` will be used to define classes,
 #' otherwise classes will be labeled `class1`, `class2` etc.
-#' @param N (integer) number of (top N) scored nodes to return
 #' @param method (character) either `nns` or `rw`. If `nns`, then values will be computed
 #' using cosine similarity, if not values are computed using spreading activation.
 #' @param beta (numeric) in (0,1), specifies the extent to which the algorithm favors local (similar labels for neighbors)
@@ -16,11 +15,11 @@
 #' of seeds and re-run algorithm. Required to get std. errors.
 #' @param num_bootstraps (integer) number of bootstraps to use.
 #' @param prop_seeds (numeric) proportion of seeds to sample when bootstrapping.
+#' @param permute (logical) if TRUE, compute empirical p-values using permutation test
+#' @param num_permutations (numeric) number of permutations to use.
 #' @param softmax (logical) if TRUE, the exponential of a node's score for a given class
 #' is normalized by the sum of the exponential of scores across all classes.
 #' Option is only available when two or more classes are specified.
-#' @param as_list (logical) if FALSE all results are combined into a single data.frame
-#' If TRUE, a list of data.frames is returned with one data.frame per class.
 #' @param verbose (logical) if TRUE show progress bar.
 #'
 #' @return a `data.frame` or list of data.frames (one for each class)
@@ -50,22 +49,22 @@
 #'
 #' # propagate label using rw
 #' rw_labels <- labelProp(x = transition_matrix, seeds = seeds,
-#' method = "rw", N = 10, beta = 0.5, as_list = TRUE)
+#' method = "rw", beta = 0.5)
 #'
 #' # propagate label using nns,
 #' # notice the main input, x, are the vector representations
-#' nns_labels <- labelProp(x = anes2016_glove, seeds = seeds,
-#' method = "nns", N = 10, as_list = TRUE)
+#' nns_labels <- labelProp(x = anes2016_glove, seeds = seeds, method = "nns")
 #'
 #' # check output for economy
 #' rw_labels[["economy"]]
 #' nns_labels[["economy"]]
 #'
-labelProp <- function(x, seeds, N = 10, method = "rw", beta = 0.5, bootstrap = FALSE, num_bootstraps = 100, prop_seeds = 0.5, softmax = FALSE, as_list = FALSE, verbose = TRUE){
+labelProp <- function(x, seeds, method = "rw", beta = 0.5, bootstrap = FALSE, num_bootstraps = 100, prop_seeds = 0.5, permute = FALSE, num_permutations = 100, softmax = FALSE, verbose = TRUE){
 
   # check object type
   if(!is.list(seeds) & !is.character(seeds)) warning('"seeds" must be a character vector or list of character vectors. \n Each characeter vector is understood to represent a single class. \n', call. = FALSE)
   if(is.character(seeds)) seeds <- list(seeds)
+  if(bootstrap && num_bootstraps < 100) stop('num_bootstraps must be at least 100', call. = FALSE) # check num_bootstraps >= 100
 
   # softmax requires two classes
   if(softmax){
@@ -91,15 +90,31 @@ labelProp <- function(x, seeds, N = 10, method = "rw", beta = 0.5, bootstrap = F
     if(!(nrow(x) == ncol(x) && all(round(rowSums(x), 0) == 1) && all(diag(x) == 0)))stop('if method = "rw", x must be a symmetric transtition matrix with rows summing to 1 and diagonal set to 0.')
 
     if (bootstrap) {
-
-      if(verbose) result <- pbapply::pbreplicate(num_bootstraps, compute_rw_score(x = x, seeds = lapply(seeds, function(s) sample(s, size = ceiling(prop_seeds*length(s)), replace = FALSE)), beta = beta, softmax = TRUE), simplify = FALSE)
-      else result <- replicate(num_bootstraps, compute_rw_score(x = x, seeds = lapply(seeds, function(s) sample(s, size = ceiling(prop_seeds*length(s)), replace = FALSE)), beta = beta, softmax = TRUE), simplify = FALSE)
-      result <- dplyr::bind_rows(result) %>% dplyr::group_by(class, node) %>% dplyr::summarize(std.error = sd(score), score = mean(score), .groups = "drop_last") %>% dplyr::select(node, class, score, std.error) %>% dplyr::ungroup()
-
+      if(verbose) cat("starting bootstrapping", "\n")
+      if(verbose) result <- pbapply::pbreplicate(num_bootstraps, compute_rw_score(x = x, seeds = lapply(seeds, function(s) sample(s, size = ceiling(prop_seeds*length(s)), replace = FALSE)), beta = beta, softmax = softmax), simplify = FALSE)
+      else result <- replicate(num_bootstraps, compute_rw_score(x = x, seeds = lapply(seeds, function(s) sample(s, size = ceiling(prop_seeds*length(s)), replace = FALSE)), beta = beta, softmax = softmax), simplify = FALSE)
+      classes <- names(result[[1]])
+      result_bs <- vector('list', length = length(classes)) %>% setNames(classes)
+      for(cl in names(result[[1]])){
+        result_bs[[cl]] <- lapply(result, "[[", cl) %>% dplyr::bind_rows(.) %>% dplyr::group_by(class, node) %>% dplyr::summarize(std.error = sd(score), score = mean(score), .groups = "drop_last") %>% dplyr::select(node, class, score, std.error) %>% dplyr::ungroup()
+      }
+      if(verbose) cat("done with bootstrapping", "\n")
     } else {
 
       result <- compute_rw_score(x = x, seeds = seeds, beta = beta, softmax = softmax)
 
+    }
+
+    if (permute) {
+      if(verbose) cat("starting permutations", "\n")
+      if(verbose) perm_tbs <- pbapply::pbreplicate(num_permutations, compute_rw_score(x = x, seeds = lapply(seeds, function(s) sample(rownames(x), length(s), replace=TRUE)), beta = beta, softmax = softmax), simplify = FALSE)
+      else perm_tbs <- replicate(num_permutations, compute_rw_score(x = x, seeds = lapply(seeds, function(s) sample(rownames(x), length(s))), beta = beta, softmax = softmax), simplify = FALSE)
+      classes <- names(result)
+      for(cl in classes){
+        p.value <- lapply(perm_tbs, function(tb) tb[[cl]] %>% dplyr::pull(score)) %>% do.call(cbind,.) %>% apply(., 2, function(j) j >= result[[cl]]$score) %>% rowSums(.)/num_permutations
+        result[[cl]] <- result[[cl]] %>% dplyr::mutate("p.value" = p.value)
+      }
+      if(verbose) cat("done with permutations", "\n")
     }
 
   } else if ( method == "nns" ) {
@@ -107,29 +122,36 @@ labelProp <- function(x, seeds, N = 10, method = "rw", beta = 0.5, bootstrap = F
     # check x is a transition matrix
     if((nrow(x) == ncol(x) && all(round(rowSums(x), 0) == 1) && all(diag(x) == 0)))warning('this looks like a transition matrix, are you sure you want to set method = "nns"?', call. = FALSE)
 
+
     if (bootstrap) {
-
+      if(verbose) cat("starting bootstrapping", "\n")
       if(verbose) result <- pbapply::pbreplicate(num_bootstraps, compute_nns_score(x = x, seeds = lapply(seeds, function(s) sample(s, size = ceiling(prop_seeds*length(s)), replace = FALSE)), softmax = softmax), simplify = FALSE)
-      else result <- replicate(num_bootstraps, compute_nns_score(x = x, seeds = lapply(seeds, function(s) sample(s, size = ceiling(prop_seeds*length(s)), replace = FALSE)), softmax = softmax), simplify = FALSE)
-      result <- dplyr::bind_rows(result) %>% dplyr::group_by(class, node) %>% dplyr::summarize(std.error = sd(score), score = mean(score), .groups = "drop_last") %>% dplyr::select(node, class, score, std.error) %>% dplyr::ungroup()
-
+      else result <- replicate(num_bootstraps, compute_rw_score(x = x, seeds = lapply(seeds, function(s) sample(s, size = ceiling(prop_seeds*length(s)), replace = FALSE)), softmax = softmax), simplify = FALSE)
+      classes <- names(result[[1]])
+      result_bs <- vector('list', length = length(classes)) %>% setNames(classes)
+      for(cl in names(result[[1]])){
+        result_bs[[cl]] <- lapply(result, "[[", cl) %>% dplyr::bind_rows(.) %>% dplyr::group_by(class, node) %>% dplyr::summarize(std.error = sd(score), score = mean(score), .groups = "drop_last") %>% dplyr::select(node, class, score, std.error) %>% dplyr::ungroup()
+      }
+      if(verbose) cat("done with bootstrapping", "\n")
     } else {
 
       result <- compute_nns_score(x = x, seeds = seeds, softmax = softmax)
 
     }
 
+    if (permute) {
+      if(verbose) cat("starting permutations", "\n")
+      if(verbose) perm_tbs <- pbapply::pbreplicate(num_permutations, compute_nns_score(x = x, seeds = lapply(seeds, function(s) sample(rownames(x), length(s), replace=TRUE)), softmax = softmax), simplify = FALSE)
+      else perm_tbs <- replicate(num_permutations, compute_rw_score(x = x, seeds = lapply(seeds, function(s) sample(rownames(x), length(s))), softmax = softmax), simplify = FALSE)
+      classes <- names(result)
+      for(cl in classes){
+        p.value <- lapply(perm_tbs, function(tb) tb[[cl]] %>% dplyr::pull(score)) %>% do.call(cbind,.) %>% apply(., 2, function(j) j >= result[[cl]]$score) %>% rowSums(.)/num_permutations
+        result[[cl]] <- result[[cl]] %>% dplyr::mutate("p.value" = p.value)
+      }
+      if(verbose) cat("done with permutations", "\n")
+    }
+
   } else stop('method must be either "rw" or "nns"')
-
-  # limit output to top N
-  if(is.numeric(N) || is.integer(N)) result <- result %>% dplyr::group_by(class) %>% dplyr::top_n(N, wt = score) %>% dplyr::ungroup() %>% dplyr::arrange(-score)
-
-  # if as_list
-  if(as_list){
-    result <- result %>% dplyr::group_by(class) %>% dplyr::group_split() %>% as.list()
-    if(is.null(names(seeds))) names(result) <- paste0("class", 1:length(seeds))
-    else names(result) <- rev(names(seeds))
-  }
 
   # output
   return(result)
@@ -173,6 +195,11 @@ compute_rw_score <- function(x, seeds, beta = 0.5, softmax = TRUE){
   result <- cbind("node" = rownames(x), score)
   result <- result %>% tidyr::pivot_longer(cols = colnames(score), names_to = "class", values_to = "score")
 
+  # as list
+  result <- result %>% dplyr::group_by(class) %>% dplyr::group_split() %>% as.list()
+  if(is.null(names(seeds))) names(result) <- paste0("class", 1:length(seeds))
+  else names(result) <- rev(names(seeds))
+
   # out
   return(result)
 }
@@ -194,6 +221,11 @@ compute_nns_score <- function(x, seeds, softmax = TRUE){
 
   # softmax: shift cosine similarity scale from -1 to 1 to to 0 - 2
   if(softmax) result <- result %>% dplyr::mutate(score = score + 1) %>% dplyr::group_by(node) %>% dplyr::mutate(score = score/sum(score)) %>% dplyr::ungroup()
+
+  # as list
+  result <- result %>% dplyr::group_by(class) %>% dplyr::group_split() %>% as.list()
+  if(is.null(names(seeds))) names(result) <- paste0("class", 1:length(seeds))
+  else names(result) <- rev(names(seeds))
 
   # out
   return(result)
